205a87a1b3852c2234d7c65efb5def64d162ca3e
vinlopant <antoniovilopez@gmail.com>
2021-10-25 09:02:43 +0200

    acabando sharepreferences

--- app/src/main/java/com/murallaromana/dam/MainActivity.kt
+++ app/src/main/java/com/murallaromana/dam/MainActivity.kt
@@ -1,18 +1,47 @@
 package com.murallaromana.dam
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btRegistrarse:Button
+    private  lateinit var textIUsuario: TextInputEditText
+    private  lateinit var textPass: TextInputEditText
+    private  lateinit var textMail: TextInputEditText
+    private  lateinit var textDireccion: TextInputEditText
+    private  lateinit var textTelefono: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btRegistrarse=findViewById(R.id.btRegistrarse)
         btRegistrarse.setOnClickListener {onBackPressed()
+
+        textIUsuario=findViewById(R.id.inputUsuario)
+        textPass=findViewById(R.id.inputPass)
+        textMail=findViewById(R.id.inputMail)
+        textDireccion=findViewById(R.id.inputDireccion)
+        textTelefono=findViewById(R.id.inputTelefono)
+
+            val sharedPref = getPreferences(Context.MODE_PRIVATE)
+            with (sharedPref.edit()) {
+                putString("Usuario", textIUsuario.text.toString())
+                putString("Password", textPass.text.toString())
+                putString("E-mail", textMail.text.toString())
+                putString("Direccion", textDireccion.text.toString())
+                putString("Telefono", textTelefono.text.toString())
+                commit()
+            }
+
+          //  val editor = sharedPref.edit()
+            //editor.putInt("", 1)
+
         }
 
     }
--- app/src/main/java/com/murallaromana/dam/MainActivity2.kt
+++ app/src/main/java/com/murallaromana/dam/MainActivity2.kt
@@ -1,21 +1,39 @@
 package com.murallaromana.dam
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity2 : AppCompatActivity() {
     private lateinit var btRegistrarse: Button
+    private lateinit var btIniciar: Button
+    private  lateinit var textIUsuario: TextInputEditText
+    private  lateinit var textIpassLogin: TextInputEditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
         btRegistrarse = findViewById(R.id.Registrarse)
+        btIniciar=findViewById(R.id.btIniciar)
         btRegistrarse.setOnClickListener {
             Toast.makeText(this, "Registro", Toast.LENGTH_SHORT).show()
-
             val intent= Intent(this, MainActivity::class.java)
             startActivity(intent)
-    }}
+    }
+
+        btIniciar.setOnClickListener{
+        textIUsuario=findViewById(R.id.inputUsuario)
+        textIpassLogin=findViewById(R.id.inputPass)
+            val sharedPref =getPreferences(MODE_PRIVATE)
+           // val defaultValue = resources.getString(R.integer.saved_high_score_default_key)
+            val highScore = sharedPref.getString("Usuario", "")
+            textIUsuario.setText(highScore)
+        }
+
+    }
 }
